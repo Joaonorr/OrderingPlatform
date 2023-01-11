56bc5af2be782399292237e36c7efc6c53cbf37b
Joaonorr <joaonoroes.jn@gmail.com>
2023-01-10 22:01:43 -0300

    feat: Domain Edit

--- src/Domain/Products/Category.cs
+++ src/Domain/Products/Category.cs
@@ -1,9 +1,11 @@
-﻿namespace OrderingPlatform.Domain.Products
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
+﻿namespace OrderingPlatform.Domain.Products;
 
-    }
+public class Category
+{
+    public int Guid { get; set; }
+    public string MyProperty { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string EditedBy { get; set; }
+    public DateTime EditedOn { get; set;}
 }
